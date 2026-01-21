19cef3111bd089062513422403a35aa45bcf6784
[SAKURUG-Pd-Shimizu] <shimizu@sakurug.co.jp>
2026-01-21 15:11:24 +0900

    自分用のブランチ

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import dockerLogo from "./assets/image/docker-mark-blue.svg";
 import reactLogo from "./assets/image/react.svg";
+import viteLogo from "./assets/image/vite.svg";
 import "./assets/styles/App.css";
 
 const App = () => {
-  return <img src={reactLogo} className="logo" alt="React logo" />;
+  return (
+    <>
+      <header>
+        <ul className="logoWrap flex justify-center gap-1">
+          <li>
+            <img src={dockerLogo} className="logo h-40" alt="Docker logo" />
+          </li>
+          <li>
+            <img src={reactLogo} className="logo h-40" alt="React logo" />
+          </li>
+          <li>
+            <img src={viteLogo} className="logo h-40" alt="Vite logo" />
+          </li>
+        </ul>
+        <h1 className="text-6xl">Docker + React + vite</h1>
+      </header>
+    </>
+  );
 };
 
 export default App;
